d0228837abd1ac034507d96d9c5c302f3fe1346e
윤이진 <483759@naver.com>
2021-07-24 18:47:28 +0900

    [feat]: ~ JPA 연관 관계 매핑 실습

--- hellojpa/src/main/java/hellojpa/Main.java
+++ hellojpa/src/main/java/hellojpa/Main.java
@@ -1,11 +1,14 @@
 package hellojpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import hellojpa.entity.Member;
+import hellojpa.entity.Team;
 
 public class Main {
 
@@ -17,11 +20,33 @@ public class Main {
 		tx.begin();
 
 		try {
+			Team team = new Team();
+			team.setName("TeamA");
+			em.persist(team);
+			
 			Member member = new Member();
-			member.setId(100L);
+			//member.setId(100L);
 			member.setName("윤이진");
-
+			member.setTeam(team);	//단방향 연관관계 설정, 참조 저장
+			//member.setMemberType(MemberType.ADMIN);
+			
 			em.persist(member);
+			
+			em.flush();
+			em.clear();
+			
+		    //조회
+		    Member findMember = em.find(Member.class, member.getId());
+		    //연관관계 생성
+		    Team findTeam = findMember.getTeam();
+		    
+		    //연관관계가 없음
+		    //Team findTeam = em.find(Team.class, team.getId());
+		    
+		    List<Member> members = findTeam.getMembers();
+		    for(Member mem: members) {
+		    	System.out.println(mem);
+		    }
 
 			tx.commit();
 		} catch (Exception e) {
--- hellojpa/src/main/java/hellojpa/entity/Member.java
+++ hellojpa/src/main/java/hellojpa/entity/Member.java
@@ -1,23 +1,59 @@
 package hellojpa.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Member {
-	@Id
+	@Id @GeneratedValue
 	private Long id;
+
+	@Column(name = "USERNAME", nullable = false, length = 20)
 	private String name;
+	private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", name=" + name + ", age=" + age + ", team=" + team + "]";
 	}
 }
